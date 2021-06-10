f751ab70f7a0fd95949611abe875d8dfce7cdf4c
lengochung <85401957+lengochung@users.noreply.github.com>
2021-06-11 00:19:28 +0700

    Add files via upload

---
+++ action.js
@@ -0,0 +1,176 @@
+
+
+//Tạo trước 3 sinh viên
+check_insert(new Student(501200001,'Phan Thành Công',20,0902525425,'CD20CT1','CN-TT'));
+check_insert(new Student(501200018,'Lê Ngọc Hưng',40,0902525425,'CD20CT2','CN-TT'));
+check_insert(new Student(501200031,'Lâm Dương An',18,0902525425,'CD20CK4','CƠ KHÍ'));
+check_insert(new Student(501200042,'Lê Thị Phương Lan',18,0902525425,'CD20CT1','CN-TT'));
+check_insert(new Student(501200111,'Nguyễn Hữu Tài',18,0902525425,'CD20KT3','KINH TẾ'));
+check_insert(new Student(501200000,'Phạm Quốc Tú',18,0902525425,'CD20CK5','CƠ KHÍ'));
+
+// Tạo biến i, k tăng để đặt tên cho thuộc tính student, del_student trong LocalStorage
+var dem1 = dem2 = 0;
+for (const key in localStorage) {
+    if(key.startsWith('student')){ dem1++;}
+    if(key.startsWith('del_student')){ dem2++;}
+}
+localStorage.i = dem1;
+localStorage.k = dem2;
+//Tạo trước 3 sinh viên
+// check_insert(new Student(501200001,'Phan Thành Công',20,0902525425,'CD20CT1','CN-TT'));
+// check_insert(new Student(501200018,'Lê Ngọc Hưng',40,0902525425,'CD20CT2','CN-TT'));
+// check_insert(new Student(501200042,'Lê Thị Phương Lan',18,0902525425,'CD20CT1','CN-TT'));
+
+// Khai báo trỏ đến bảng sinh viên và bảng xóa nhân viên
+var student_pane = document.getElementById('student_pane');
+var student_del = document.getElementById('student_del');
+
+// Load Student
+function loadStudent(){
+    student_pane.innerHTML = `<tr><td colspan='6'><h3>Thông tin sinh viên</h3></td></tr>
+                                <tr>
+                                    <td>STT</td>
+                                    <td>MSSV</td>
+                                    <td>Họ tên</td>
+                                    <td>Tuổi</td>
+                                    <td>Số điện thoại</td>
+                                    <td>Lớp</td>
+                                    <td>Khoa</td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>`;
+    student_del.innerHTML = ` <tr><td colspan='6'><h3>Chờ duyệt</h3></td></tr>
+                            <tr>
+                                <td>STT</td>
+                                <td>MSSV</td>
+                                <td>Họ tên</td>
+                                <td>Tuổi</td>
+                                <td>Số điện thoại</td>
+                                <td>Lớp</td>
+                                <td>Khoa</td>
+                                <td onclick='restore_all()'><a style="color: grey;" href="#">Hoàn tác tất cả</a></td>
+                                <td onclick='delete_all()'><a style="color: red;" href="#">Xóa tất cả</a></td>
+                            </tr>`;
+    re_arraySVfromLocalStorage('student').forEach((element, i) => {
+        student_pane.innerHTML += `<tr>
+                                        <td>${i+1}</td>
+                                        <td>${element.id}</td>
+                                        <td>${element.name}</td>
+                                        <td>${element.age}</td>
+                                        <td>${element.phone}</td>
+                                        <td>${element.class}</td>
+                                        <td>${element.branch}</td>
+                                        <td onclick="edit(this)"><a href="#">Chỉnh sửa</a></td>
+                                        <td onclick="remove_restore(this,'student','del_student')"><a href="#" style="color: red;">Loại bỏ</a></td>
+                                    </tr>`;
+    });
+    re_arraySVfromLocalStorage('del_student').forEach((element, i) => {
+        student_del.innerHTML += `<tr>
+                                        <td>${i+1}</td>
+                                        <td>${element.id}</td>
+                                        <td>${element.name}</td>
+                                        <td>${element.age}</td>
+                                        <td>${element.phone}</td>
+                                        <td>${element.class}</td>
+                                        <td>${element.branch}</td>
+                                        <td onclick="remove_restore(this,'del_student','student')"><a href="#" style="color: grey;">Hoàn tác</a></td>
+                                        <td onclick='deleteOut(this)'><a href="#" style="color: red;">Xóa</a></td>
+                                    </tr>`;
+    });
+    re_ValueOptionOfSelect(document.getElementsByClassName('select')[0],'branch');
+    re_ValueOptionOfSelect(document.getElementsByClassName('select')[1],'class');
+    displayTotalStudent(document.getElementsByClassName('display')[0],'student');
+    displayTotalStudent(document.getElementsByClassName('display')[1],'del_student');
+   
+    $('#sort').css('width','25%');
+    
+}
+
+
+
+
+
+
+
+
+
+// Tìm kiếm tên, ID
+function re_search_nameID(search){
+    student_pane.innerHTML = "<tr><td colspan='6'><h3>Kết quả tìm kiếm</h3></td></tr>";
+    re_arraySVfromLocalStorage('student').forEach((element, i) => {
+        if(search==element.id||element.name.search(search)>=0){
+            student_pane.innerHTML += `<tr>
+                                        <td></td>
+                                        <td>${element.id}</td>
+                                        <td>${element.name}</td>
+                                        <td>${element.age}</td>
+                                        <td>${element.phone}</td>
+                                        <td>${element.class}</td>
+                                        <td>${element.branch}</td>
+                                        <td onclick="edit_search(this)"><a href="#">Chỉnh sửa</a></td>
+                                        <td onclick="remove_restore(this,'student','del_student')"><a href="#" style="color: red;">Loại bỏ</a></td>
+                                    </tr>`;
+        }
+    });
+}
+
+
+// Lọc
+function re_filter(filter){
+    document.getElementById('search').value = "";
+    student_pane.innerHTML = "<tr><td colspan='6'><h3>Đã lọc</h3></td></tr>";
+    if(filter[0].value=="Chọn"){
+        if(filter[1].value=="Chọn"){ loadStudent(); }
+        else {
+            re_arraySVfromLocalStorage('student').forEach(element => {
+                if(filter[1].value==element.class){
+                    student_pane.innerHTML += `<tr>
+                                                    <td></td>
+                                                    <td>${element.id}</td>
+                                                    <td>${element.name}</td>
+                                                    <td>${element.age}</td>
+                                                    <td>${element.phone}</td>
+                                                    <td>${element.class}</td>
+                                                    <td>${element.branch}</td>
+                                                    <td onclick="edit_search(this)"><a href="#">Chỉnh sửa</a></td>
+                                                    <td onclick="remove_restore(this,'student','del_student')"><a href="#" style="color: red;">Loại bỏ</a></td>
+                                                </tr>`;
+                } 
+            });
+        } 
+    } else {
+        if(filter[1].value=="Chọn"){
+            re_arraySVfromLocalStorage('student').forEach(element => {
+                if(filter[0].value==element.branch){
+                    student_pane.innerHTML += `<tr>
+                                                    <td></td>
+                                                    <td>${element.id}</td>
+                                                    <td>${element.name}</td>
+                                                    <td>${element.age}</td>
+                                                    <td>${element.phone}</td>
+                                                    <td>${element.class}</td>
+                                                    <td>${element.branch}</td>
+                                                    <td onclick="edit_search(this)"><a href="#">Chỉnh sửa</a></td>
+                                                    <td onclick="remove_restore(this,'student','del_student')"><a href="#" style="color: red;">Loại bỏ</a></td>
+                                                </tr>`;
+                } 
+            });
+        } else {
+            re_arraySVfromLocalStorage('student').forEach(element => {
+                if(filter[0].value==element.branch&&filter[1].value==element.class){
+                    student_pane.innerHTML += `<tr>
+                                                    <td></td>
+                                                    <td>${element.id}</td>
+                                                    <td>${element.name}</td>
+                                                    <td>${element.age}</td>
+                                                    <td>${element.phone}</td>
+                                                    <td>${element.class}</td>
+                                                    <td>${element.branch}</td>
+                                                    <td onclick="edit_search(this)"><a href="#">Chỉnh sửa</a></td>
+                                                    <td onclick="remove_restore(this,'student','del_student')"><a href="#" style="color: red;">Loại bỏ</a></td>
+                                                </tr>`;
+                } 
+            });
+        }
+    }
+}
---
+++ function.js
@@ -0,0 +1,249 @@
+// Đinh nghĩa lớp student
+function Student(...rest){
+    this.id = rest[0],
+    this.name = rest[1],
+    this.age = rest[2],
+    this.phone = rest[3],
+    this.class = rest[4],
+    this.branch = rest[5]
+}
+var tv_array = ['MSSV','Tên Sinh Viên','Tuổi','Số điện thoại','Lớp','Khoa'];
+var attr_array = ['id','name_student','age','phone','class','branch'];
+
+// Thêm SV vào mảng localStorage
+var i = 0;
+function add_SVintoLocalStorage(){
+    let id = check_empty(document.getElementById('id'));
+    let name_student = check_empty(document.getElementById('name_student'));
+    let age = check_empty(document.getElementById('age'));
+    let phone = check_empty(document.getElementById('phone'));
+    let classs = check_empty(document.getElementById('class'));
+    let branch = check_empty(document.getElementById('branch'));
+    if(i==0){
+        check_insert(new Student(id.value,name_student.value,age.value,phone.value,classs.value,branch.value));
+    } else { alert('Thất bại \nKhông trường nào được bỏ trống'); }
+    i = 0;
+}
+
+// Kiểm tra dữ liệu rỗng
+function check_empty(input){
+    if(input.value==''){
+        input.setAttribute('placeholder','... không được để trống');
+        i++;
+    } return input;
+}
+
+
+
+
+
+// CHỉnh sửa tại mảng localStorage
+function edit(td){
+    let id = td.parentElement.getElementsByTagName('td')[1].innerText;
+    for (const key in localStorage) {
+        if(key.startsWith('student')&&id==JSON.parse(localStorage[key]).id){
+            let js = JSON.parse(localStorage[key]), i = 0;
+            for (const attr in js) {
+                js[attr] = prompt(tv_array[i],js[attr]); i++;
+            }
+            localStorage[key] = JSON.stringify(js);
+            break;
+        }
+    }
+    loadStudent();
+}
+
+function edit_search(td){
+    let id = td.parentElement.getElementsByTagName('td')[1].innerText;
+    let search = "";
+    for (const key in localStorage) {
+        if(key.startsWith('student')&&id==JSON.parse(localStorage[key]).id){
+            let js = JSON.parse(localStorage[key]), i = 0;
+            for (const attr in js) {
+                js[attr] = prompt(tv_array[i],js[attr]);
+                if(i==0){ search = js[attr];}   
+                i++;
+            }
+            localStorage[key] = JSON.stringify(js);
+            break;
+        }
+    }
+    re_search_nameID(search);
+}
+
+
+// Lấy dữ liêu từ localStorage trả về mảng
+function re_arraySVfromLocalStorage(value){
+    var arr = new Array();
+    for (const key in localStorage) {
+        if(key.startsWith(value)){
+            arr.push(JSON.parse(localStorage[key]));
+        }
+    }
+    return arr;
+}
+
+
+
+// Xóa sinh viên
+function remove_restore(td, value1, value2){
+    let id = td.parentElement.getElementsByTagName('td')[1].innerText;
+    for (const key in localStorage) {
+        if(key.startsWith(value1)&&id==JSON.parse(localStorage[key]).id){
+            if (value1=='student') {
+                localStorage[value2 + localStorage.k] = localStorage[key];
+                localStorage.k++;
+            } else {
+                localStorage[value2 + localStorage.i] = localStorage[key];
+                localStorage.i++;
+            }
+            localStorage.removeItem(key);
+        }
+    }
+    loadStudent();
+}
+
+// Hàm kiểm tra nhập trùng
+function check_insert(object){
+    var isval = true;
+    for (let i=0; i<re_arraySVfromLocalStorage('student').length; i++) {
+        if(object.id==re_arraySVfromLocalStorage('student')[i].id){
+            isval = false;
+            break;
+        }
+    }
+    for (let i=0; i<re_arraySVfromLocalStorage('del_student').length; i++) {
+        if(object.id==re_arraySVfromLocalStorage('del_student')[i].id){
+            isval = false;
+            break;
+        }
+    }
+    if(isval){
+        localStorage['student' + localStorage.i] = JSON.stringify(object);
+        localStorage.i++;
+        alert('Thêm thành công');
+        setValueforFormInput("");
+    } else { alert('MSSV đã tồn tại trong hệ thống'); }
+}
+ // Trả về rỗng cho các Form nhập
+function setValueforFormInput(valueSet){
+    for (const iterator of document.getElementsByClassName('null_value')) { iterator.setAttribute('placeholder',valueSet)}
+}
+
+
+
+
+
+
+
+// Xóa khỏi LocalStorage
+function deleteOut(td) {
+   let id = td.parentElement.getElementsByTagName('td')[1].innerText;
+   for (const key in localStorage) {
+       if (key.startsWith('del_student')&&id==JSON.parse(localStorage[key]).id) {
+           localStorage.removeItem(key);
+       }
+   }
+   loadStudent();
+}
+// Xóa tất cả khỏi thùng rác sinh viên
+function delete_all(){
+    for (const key in localStorage) {
+        if (key.startsWith('del_student')) {  localStorage.removeItem(key); }
+    }
+    loadStudent();
+}
+// Đưa tất cả sinh viên hoàn tác
+function restore_all(){
+    for (const key in localStorage) {
+        if (key.startsWith('del_student')) {
+            localStorage['student' + localStorage.i] = localStorage[key];
+            localStorage.i++;
+            localStorage.removeItem(key);
+        }
+    }
+    loadStudent();
+}
+
+
+
+// Lấy thông tin Khoa và Lớp
+function re_ArrayGetValueAttr(attr){
+    let arr = new Array('Chọn');
+        re_arraySVfromLocalStorage('student').forEach(element => {
+            for (const key in element) {
+               if(key==attr){
+                   let isval = true;;
+                   for (const iterator of arr) {
+                       if(element[key]==iterator){ isval = false; break; }
+                   }
+                   if(isval){
+                        arr.push(element[key]);
+                   }
+               }
+            }
+        });
+    return arr;
+}
+
+
+
+// getValueOptionOfSelect
+function re_ValueOptionOfSelect(select, attr){
+    select.innerHTML = "";
+    for (const iterator of re_ArrayGetValueAttr(attr)) {
+        select.innerHTML += `<option value="${iterator}">${iterator}</option>`;
+    }
+}
+
+
+
+// Đếm số sinh viên
+function  countStudent(array){
+    let count = 0;
+    array.forEach(element => {
+        count++;
+    });
+    return count;
+}
+
+
+// Hiển thị số sinh viên
+function displayTotalStudent(params1, params2){
+    params1.innerHTML = "";
+    params1.innerHTML += `(<font color="red"><b>${countStudent(re_arraySVfromLocalStorage(params2))}</b></font>)` 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
